4444ea8bc3e92b031f735d9906546cfe068b6ddb
captainAyan <ayanchakraborty105@gmail.com>
2021-06-10 13:17:47 +0530

    build: :hammer: Fixed build scripts
    Executables were not running on some computers due to .dll issues. Fixed those issues.

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ *.cpp -o accountlite.exe -std=c++17
+g++ *.cpp -o accountlite.exe -std=c++17 -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic
 
 ./accountlite.exe
 
