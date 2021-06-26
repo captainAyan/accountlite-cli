75774f03df353289118be96dc4a3f515a37e6eb4
captainAyan <ayanchakraborty105@gmail.com>
2021-06-26 14:05:54 +0530

    refactor: :zap: Removed unnecessary filter from journal statment method
    The journalEntries method used to create a filtered journal entries list, and then printed them out; now removed that filtered list (and it's creation process) as it's not really required.

--- statement.h
+++ statement.h
@@ -71,25 +71,6 @@ void trialBalance(std::vector<Journal>* journalList,
 void journalEntries(std::vector<Journal>* journalList, 
   std::map<std::string, std::string>* metaDataMap, int from_time, int to_time) {
 
-  // filter journal with from_time and to_time
-  std::vector<Journal> filteredJournalList;
-
-  // if this condition is true, then filtering is not required (for optimization)
-  if(from_time == 0 && 
-    to_time > journalList->at(journalList->size()-1).getTime()) {
-    filteredJournalList = *journalList;
-  }
-  else {
-    // filtering process
-    for (size_t i = 0; i < journalList->size(); i++) {
-      if (journalList->at(i).getTime() > from_time && 
-          journalList->at(i).getTime() < to_time) {
-        filteredJournalList.push_back(journalList->at(i));
-      }
-      if (journalList->at(i).getTime() > to_time) break;
-    }
-  }
-  
   clitable::Table table;
   short particular_column_size = 30;
 
@@ -109,19 +90,26 @@ void journalEntries(std::vector<Journal>* journalList,
 
   for (size_t i = 0; i < 4; i++) table.addColumn(c[i]);
 
-  for (size_t i = 0; i < filteredJournalList.size(); i++) {
-    std::string particulars = filteredJournalList.at(i).getDebit() + " A/c";
-    particulars += dots((particular_column_size - filteredJournalList.at(i).getDebit().size())-7) + "Dr.";
-    particulars += "To. " + filteredJournalList.at(i).getCredit() + " A/c\n";
-    particulars += "(" + filteredJournalList.at(i).getNarration() + ")";
+  for (size_t i = 0; i < journalList->size(); i++) {
 
-    std::string r[4] {
-      timestampToString(filteredJournalList.at(i).getTime()),
-      particulars,
-      currency+formatCurrency(filteredJournalList.at(i).getAmount(), currencyFormat),
-      "\n"+currency+formatCurrency( filteredJournalList.at(i).getAmount(), currencyFormat)
-    };
-    table.addRow(r);
+    if (journalList->at(i).getTime() > from_time && 
+      journalList->at(i).getTime() < to_time) {
+
+      std::string particulars = journalList->at(i).getDebit() + " A/c";
+      particulars += dots((particular_column_size - journalList->at(i).getDebit().size())-7) + "Dr.";
+      particulars += "To. " + journalList->at(i).getCredit() + " A/c\n";
+      particulars += "(" + journalList->at(i).getNarration() + ")";
+
+      std::string r[4] {
+        timestampToString(journalList->at(i).getTime()),
+        particulars,
+        currency+formatCurrency(journalList->at(i).getAmount(), currencyFormat),
+        "\n"+currency+formatCurrency( journalList->at(i).getAmount(), currencyFormat)
+      };
+      table.addRow(r);
+    }
+    else if(journalList->at(i).getTime() > to_time) break;
+
   }
   table.draw();
 }
